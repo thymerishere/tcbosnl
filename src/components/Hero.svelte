3c44fb1adea85fc5f38c2f8afd3b7e5eb5e9ad32
Thomas Bos <tbos@infiniot.nl>
2024-07-01 22:14:47 +0200

    Fix hero spacing

--- src/components/Hero.svelte
+++ src/components/Hero.svelte
@@ -1,14 +1,14 @@
 <span id="home" class="absolute -top-10"></span>
 
 <div class="bg-hero bg-no-repeat bg-cover bg-center">
-    <div class="pb-10 pt-40 px-10 max-w-[500px]">
+    <div class="pb-10 pt-40 px-10 max-w-[600px]">
         <h2 class="py-5 text-5xl sm:text-6xl md:text-7xl lg:text-8xl mx-auto w-full">
-            <span class="font-light text-slate-100">Thomas Christiaan Bos</span>
+            <span class="font-light text-slate-100">Thomas<br>Christiaan<br>Bos</span>
         </h2>
         <p class="font-light text-slate-50 text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
-            <span>Software Engineer</span>,
-            <span>Data Scientist</span>,
-            <span>Muziekliefhebber</span>.
+            <span>Software Engineer</span>,<br>
+            <span>Data Scientist</span>,<br>
+            <span>Muziekliefhebber</span>.<br>
         </p>
     </div>
 </div>
